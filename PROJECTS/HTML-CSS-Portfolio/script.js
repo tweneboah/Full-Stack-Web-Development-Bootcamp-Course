a15345075570aebb91896a73b3153f487726c7b2
Masynctech coding School <masynctech@gmail.com>
2024-12-06 06:20:19 +0000

    HTML-CSS Portfolio

---
+++ PROJECTS/HTML-CSS-Portfolio/script.js
@@ -0,0 +1,7 @@
+//mobile toggle
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
